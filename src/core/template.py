e700889359f220d93ec61ec626d8651337d64190
Keniis0712 <keniis@126.com>
2025-11-01 19:47:39 +0800

    Bug fix

--- src/core/template.py
+++ src/core/template.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from starlette.templating import Jinja2Templates
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=pathlib.Path(__file__).parent.parent / "templates")
